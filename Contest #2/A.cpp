b2081afb52f49c9e1d195d39d4561736a253020d
Yousha <joshgates2580@gmail.com>
2026-01-26 18:48:04 +0600

    Add files via upload

---
+++ Contest #2/A.cpp
@@ -0,0 +1,17 @@
+// Question - A
+
+#include<bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+    optimize();
+
+    long a, b;
+    cin >> a >> b;
+    if(a > b) cout << a-b << endl;
+    else cout << "0" << endl;
+    return 0;
+}
