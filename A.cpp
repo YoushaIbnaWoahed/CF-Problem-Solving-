96af6bdad947aa299fc3ef0acad5b463c4976e9d
Yousha <joshgates2580@gmail.com>
2026-01-27 23:32:29 +0600

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,17 @@
+// Question - A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define optimize() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+    optimize();
+
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << s1.length() << " " << s2.length() << endl;
+    cout << s1 << " " << s2 << endl;
+    return 0;
+}
